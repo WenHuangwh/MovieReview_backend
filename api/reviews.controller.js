ab7f04d5f7a9dc95b507cb7da690ca19c55439da
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-06-13 20:49:01 -0400

    6-13

---
+++ api/reviews.controller.js
@@ -0,0 +1,7 @@
+export default class ReviewsController {
+    static async apiPostReview(req, res, next) {}
+
+    static async apiUpdateReview(req, res, next) {}
+
+    static async apiDeleteReview(req, res, next) {}
+}
